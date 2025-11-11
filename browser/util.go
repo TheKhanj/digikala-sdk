cd48c8e7d94d65904269fa7e94236a3c2c637265
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-11-11 12:29:39 +0330

    remove unused function

--- browser/util.go
+++ browser/util.go
@@ -1,24 +1,10 @@
 package browser
 
 import (
-	"context"
 	"os/exec"
 
 	"github.com/chromedp/chromedp"
 )
-
-func NewTab(ctx context.Context) (context.Context, context.CancelFunc) {
-	allocCtx, c1 := chromedp.NewExecAllocator(ctx, SensibleExecPath())
-
-	tab, c2 := chromedp.NewContext(allocCtx)
-
-	c := func() {
-		c2()
-		c1()
-	}
-
-	return tab, c
-}
 
 func SensibleExecPath() chromedp.ExecAllocatorOption {
 	locations := []string{
